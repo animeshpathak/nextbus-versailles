3b67cdb45061680f6e98f40634f4fab0a9af6948
Animesh <animesh@animeshpathak.in>
2012-07-24 15:30:51 +0200

    Addresses Issue #10

---
+++ android/src/in/animeshpathak/nextbus/Constants.java
@@ -0,0 +1,14 @@
+package in.animeshpathak.nextbus;
+
+/**
+ * The class to hold some app-wide constants
+ * @author pathak
+ */
+public class Constants {
+
+	public static final String SELECTED_LINE = "SELECTED_LINE";
+	public static final String SELECTED_STOP = "SELECTED_STOP";
+	
+	
+
+}
